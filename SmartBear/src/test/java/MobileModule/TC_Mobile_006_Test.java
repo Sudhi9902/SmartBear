01a1d53cc0781a743e7ba8445e5784682b523718
TECQNIO <TECQNIO@DESKTOP-P7OORB0>
2023-01-24 13:25:09 +0530

    Newly Created Files By Sudarshan

---
+++ SmartBear/src/test/java/MobileModule/TC_Mobile_006_Test.java
@@ -0,0 +1,33 @@
+package MobileModule;
+
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import FrameWorkLibrary.Basedata;
+import FrameWorkLibrary.PomRepoForSmartBear;
+
+/***
+ * 
+ * @author sudarshan
+ *
+ */
+public class TC_Mobile_006_Test extends Basedata {
+	@Test(groups = "Functionality")
+	// TestCase6: Verify That user is able to click on the Relevence dropdown option
+	public void TC_006_RelevenceOptionSelection() {
+		PomRepoForSmartBear psm = new PomRepoForSmartBear(driver);
+		psm.getMobilePhoneNavBar().click();
+		WebElement drop = psm.getRelevencedropDown();
+		drop.click();
+		Reporter.log("Relevence Dropdown is displayed", true);
+		psm.getOptionDrop2().click();
+		Reporter.log("User click relevence dropdown Options Sucessfully :" + " " + psm.getOptionDrop2().getText(),
+				true);
+		String dropTittle = "Name: Z to A";
+		Assert.assertEquals(psm.getOptionDrop2().getText(), dropTittle,
+				"User Not able to click on the optionalDropdown");
+	}
+}
